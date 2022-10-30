fff77b86e33e252ccef48da15ef817a1433d4518
qnguyen95 <quangtri.nguyen@dxc.com>
2022-10-30 23:30:41 +0700

    #Setup Project

---
+++ E-WebParent/E-Client/src/main/java/com/aptech/EClientApplication.java
@@ -0,0 +1,13 @@
+package com.aptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EClientApplication.class, args);
+	}
+
+}
