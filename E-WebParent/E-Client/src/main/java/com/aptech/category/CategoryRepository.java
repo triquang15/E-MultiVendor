124ffaeb89d373bb016508d7ea7938ee98f47f76
KhanhLocVo <vokhanhloc1994@gmail.com>
2022-11-03 13:41:39 +0700

    # Update Manager Shop Admin SS2

--- E-WebParent/E-Client/src/main/java/com/aptech/category/CategoryRepository.java
+++ E-WebParent/E-Client/src/main/java/com/aptech/category/CategoryRepository.java
@@ -12,6 +12,13 @@ public interface CategoryRepository extends CrudRepository<Category, Integer> {
 	@Query("SELECT c FROM Category c WHERE c.enabled = true ORDER BY c.name ASC")
 	public List<Category> findAllEnabled();
 	
+
+	/*
+	 * @Query("SELECT c FROM Category c WHERE c.enabled = true AND c.parent_id is null ORDER BY c.name ASC"
+	 * ) public List<Category> findAllNoParent();
+	 */
+	 
+
 	@Query("SELECT c FROM Category c WHERE c.enabled = true AND c.alias = ?1")
 	public Category findByAliasEnabled(String alias);
 }
